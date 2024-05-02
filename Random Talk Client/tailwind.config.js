cba75d13aec194a002a46f9f4c567c50ccc2319f
FrancoSMiño <frankow222@gmail.com>
2024-05-02 14:06:03 -0300

    initial setup

---
+++ Random Talk Client/tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {colors: {
+      'gray-100':  '#E3E3E3',
+      'gray-200':  '#CACACA',
+      'gray-300':  '#B0B0B0',
+      'gray-400':  '#969696',
+      'gray-500':  '#7C7C7C',
+      'gray-600':  '#626262',
+      'gray-700':  '#484848',
+      'gray-800':  '#2E2E2E',
+      'gray-900':  '#141414',
+
+      'grape-100': 'D8C9EE',
+      'grape-200': 'BEA6E2',
+      'grape-300': 'A382D5',
+      'grape-400': '885EC8',
+      'grape-500': '6D3ABB',
+      'grape-600': '592F98',
+      'grape-700': '442475',
+      'grape-800': '301952',
+      'grape-900': '1B0E2F',
+      
+      'aqua-100': 'E5FFF8',
+      'aqua-200': 'ACFFE8',
+      'aqua-300': '73FFD8',
+      'aqua-400': '3AFFC8',
+      'aqua-500': '00FFB7',
+      'aqua-600': '00CA91',
+      'aqua-700': '00946A',
+      'aqua-800': '005F44',
+      'aqua-900': '00291D',
+    }},
+  },
+  plugins: [],
+}
+
