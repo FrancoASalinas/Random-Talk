e128426accd311345a19735dbdfb339cda62d76b
FrancoSMiño <frankow222@gmail.com>
2024-05-16 12:00:36 -0300

    deps: cypress

--- Random Talk Client/src/lib/routes.tsx
+++ Random Talk Client/src/lib/routes.tsx
@@ -1,7 +1,11 @@
 import { Route, createRoutesFromElements } from "react-router-dom";
 import Welcome from "../pages/Welcome";
+import Chat from "../pages/Chat";
 
 export default createRoutesFromElements(
+  <>
     <Route path='/' element={<Welcome />} >
     </Route>
+    <Route element={<Chat />} path='/chat' />
+  </>
   )
