e62077a6757bac23446749fe5290fd76782de402
FrancoSMiño <frankow222@gmail.com>
2024-05-20 15:19:38 -0300

    not finished: chat. Impossible to e2e test with actual setup, resume work in a future

--- Random Talk Client/cypress/e2e/chat.cy.ts
+++ Random Talk Client/cypress/e2e/chat.cy.ts
@@ -1,22 +1,28 @@
 import { io } from "socket.io-client";
 
 it("Should show spinner while connecting", () => {
-  cy.visit("/chat");
+  cy.visit("http://127.0.0.1:5173/chat");
   cy.get('[data-test-id="spinner"]');
 });
 
-it("Should connect if there is another user in queue", async () => {
-  cy.visit("/chat");
+it("Should show an error if no other user is in queue for 30 seconds", () => {
+  cy.visit("http://127.0.0.1:5173/chat");
+  cy.wait(31000);
+  cy.contains("Seems like there aren't other people willing to talk")
+})
 
-  const socket = io("http://localhost:3000");
+// it("Should connect if there is another user in queue", async () => {
+//   cy.visit("http://127.0.0.1:5173/chat");
 
-  await new Promise<void>(resolve => {
-    socket.on("connect", () => {
-      cy.get('[data-test-id="chat-input"');
-      resolve();
-    });
-  }).then(() => socket.disconnect());
-});
+//   const socket = io("http://localhost:3000");
+
+//   await new Promise<void>(resolve => {
+//     socket.on("connect", () => {
+//       cy.get('[data-test-id="chat-input"');
+//       resolve();
+//     });
+//   }).then(() => socket.disconnect());
+// });
 
 // it("Should show chat after connecting", () => {
 //   cy.visit("/chat");
